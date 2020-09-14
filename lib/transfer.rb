d8ef31b48895c9cd3787e21c9f1a1effa52f1a08
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-14 15:10:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   
    def execute_transaction
     if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance = balance - amount
+      sender.balance = @balance - amount
       receiver.deposit(self.amount)
       self.status = "complete"
     else
